298b61ea402a062d802614e76c9115527a7e07e9
zfergus <zfergus@nyu.edu>
2019-12-27 01:37:02 -0500

    Fixed name collision in custom_download_project

--- cmake/EigenGurobiDownloadExternal.cmake
+++ cmake/EigenGurobiDownloadExternal.cmake
@@ -8,7 +8,7 @@ else()
   set(EIGEN_GUROBI_EXTRA_OPTIONS "")
 endif()
 
-function(custom_download_project name)
+function(eigen_gurobi_download_project name)
   download_project(
     PROJ         ${name}
     SOURCE_DIR   ${EIGEN_GUROBI_EXTERNAL}/${name}
@@ -23,7 +23,7 @@ endfunction()
 
 # Eigen
 function(download_eigen)
-	custom_download_project(eigen
+	eigen_gurobi_download_project(eigen
 		GIT_REPOSITORY https://github.com/eigenteam/eigen-git-mirror.git
 		GIT_TAG        3.3.7
 	)
@@ -31,7 +31,7 @@ endfunction()
 
 # Catch2
 function(download_catch2)
-    custom_download_project(Catch2
+    eigen_gurobi_download_project(Catch2
         GIT_REPOSITORY https://github.com/catchorg/Catch2.git
         GIT_TAG        v2.11.0
     )
